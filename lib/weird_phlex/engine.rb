5c416e0aa5971377e1322995d2dcc5398397a079
H3BCKN <ka.koszewski@gmail.com>
2024-08-06 22:03:47 +0200

    replace Railite with Engine"

---
+++ lib/weird_phlex/engine.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module WeirdPhlex # :nodoc:
+  class Railtie < ::Rails::Engine # :nodoc:
+    isolate_namespace WeirdPhlex
+
+  end
+end
--- lib/weird_phlex/railtie.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module WeirdPhlex # :nodoc:
-  class Railtie < ::Rails::Railtie # :nodoc:
-  end
-end
