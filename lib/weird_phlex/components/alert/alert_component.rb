602b7684c72da9596082a32c362283d61145213d
H3BCKN <ka.koszewski@gmail.com>
2024-08-07 02:10:00 +0200

    add base components

---
+++ lib/weird_phlex/components/alert/alert_component.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module WeirdPhlex
+  class AlertComponent < BaseComponent
+    def initialize(name:)
+      @name = name
+    end
+  
+    def template
+      h1 { "👋 Hello #{@name}!" }
+    end
+  end
+end 
---
+++ lib/weird_phlex/components/base_component.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+require 'phlex'
+
+module WeirdPhlex
+  class BaseComponent < Phlex::HTML
+  end
+end 
